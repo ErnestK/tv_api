82f77cb4855aea1ee97110e9a692d8dd11f9640a
Ernest Khasanzhinov <ernest.khasanzhinov@workato.com>
2025-09-02 19:40:58 +0200

    update apipie

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,10 @@
 Apipie.configure do |config|
-  config.app_name                = "TvApi"
-  config.api_base_url            = "/api"
+  config.app_name                = "TV API"
+  config.app_info                = "Streaming Content Management System"
+  config.api_base_url            = "/api/v1"
   config.doc_base_url            = "/apipie"
-  # where is your API defined?
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
+  config.validate                = false
+  config.reload_controllers      = Rails.env.development?
+  config.default_version         = "v1"
 end
